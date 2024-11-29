37d8119443634961d65be5927f591f7f4874ed1e
Marcel <numax.cz@protonmail.ch>
2024-11-29 13:06:51 +0100

    Update angularu

--- SmartUmbrellaApp/src/app/app.component.ts
+++ SmartUmbrellaApp/src/app/app.component.ts
@@ -3,9 +3,10 @@ import {Platform} from "@ionic/angular";
 import {StatusBar, Style} from "@capacitor/status-bar";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
+    selector: 'app-root',
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.scss'],
+    standalone: false
 })
 export class AppComponent {
   private SplashScreen: any;
--- SmartUmbrellaApp/src/app/home/home.page.ts
+++ SmartUmbrellaApp/src/app/home/home.page.ts
@@ -4,9 +4,10 @@ import {OpenWeatherService} from "./services/OpenWeatherService";
 
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
+    standalone: false
 })
 export class HomePage {
   public weather_data: any = null;

2bcbf57b5c28e1f922971d3b8139f0d8eb9dd248
FilipSuch <98059283+filsuch@users.noreply.github.com>
2024-11-29 15:07:14 +0100

    update

--- ESP32/SmartUmbrella/src/main.cpp
+++ ESP32/SmartUmbrella/src/main.cpp
@@ -2,39 +2,63 @@
 #include <Adafruit_SSD1306.h>
 #include <SPI.h>
 #include <DHT.h>
- #include <Adafruit_Sensor.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+#include <MPU9250.h>
 
-#define SCREEN_WIDTH 128  // Šířka displeje v pixelech
-#define SCREEN_HEIGHT 64  // Výška displeje v pixelech
-
-// Definice pinů
-#define OLED_MOSI 23    // Pin pro MOSI
-#define OLED_SCK 18     // Pin pro SCK
-#define OLED_DC 21      // Pin pro Data/Command
-#define OLED_CS 22      // Pin pro Chip Select
-#define OLED_RESET 15   // Pin pro reset
+// Definice pinů pro displej
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_MOSI 23
+#define OLED_SCK 18
+#define OLED_DC 21
+#define OLED_CS 22
+#define OLED_RESET 15
 
 // Inicializace displeje
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, OLED_MOSI, OLED_SCK, OLED_DC, OLED_RESET, OLED_CS);
 
-
-
 // Nastavení DHT11
-#define DHTPIN 14         // Pin, na který je připojen DHT11
-#define DHTTYPE DHT11     // Typ senzoru
+#define DHTPIN 14
+#define DHTTYPE DHT11
 DHT dht(DHTPIN, DHTTYPE);
+
+// Nastavení RGB LED
+#define RED_PIN 25
+#define GREEN_PIN 26
+#define BLUE_PIN 27
+
+// Inicializace MPU9250
+MPU9250 mpu;
+
+// Nastavení uspávacího režimu
+bool isSleeping = false;
+const int motionThreshold = 100; // Minimální hodnota pohybu pro aktivaci
 
 void setup() {
     // Inicializace sériové komunikace
     Serial.begin(115200);
-    
+
     // Inicializace OLED displeje
-    display.begin(SSD1306_SWITCHCAPVCC, OLED_CS);  // Upravte inicializaci pro SPI
+    display.begin(SSD1306_SWITCHCAPVCC, OLED_CS);
     display.clearDisplay();
-    
+
     // Inicializace DHT11
     dht.begin();
-    
+
+    // Inicializace RGB LED
+    pinMode(RED_PIN, OUTPUT);
+    pinMode(GREEN_PIN, OUTPUT);
+    pinMode(BLUE_PIN, OUTPUT);
+    setRGB(0, 0, 0); // Vypnout LED
+
+    // Inicializace MPU9250
+    Wire.begin();
+    mpu.begin();
+    mpu.setGyroRange(MPU9250::GYRO_RANGE_500DPS);
+    mpu.setAccelRange(MPU9250::ACCEL_RANGE_2G);
+    mpu.setDlpfBandwidth(MPU9250::DLPF_BANDWIDTH_10HZ);
+
     // Zobrazení úvodního textu
     display.setTextSize(1);
     display.setTextColor(SSD1306_WHITE);
@@ -45,6 +69,29 @@ void setup() {
 }
 
 void loop() {
+    // Kontrola pohybu
+    VectorFloat acceleration = mpu.readAccelerometer();
+    int movement = sqrt(acceleration.XAccel * acceleration.XAccel + 
+                        acceleration.YAccel * acceleration.YAccel + 
+                        acceleration.ZAccel * acceleration.ZAccel);
+
+    // Pokud je pohyb větší než threshold
+    if (movement > motionThreshold) {
+        isSleeping = false;
+        displayValues();
+        setRGB(255, 0, 0); // Změnit barvu na červenou
+    } else {
+        isSleeping = true;
+        setRGB(0, 0, 0); // Vypnout LED
+        display.clearDisplay();
+        display.display();
+    }
+
+    // Čekání před dalším čtením
+    delay(200);
+}
+
+void displayValues() {
     // Načtení hodnot teploty a vlhkosti
     float humidity = dht.readHumidity();
     float temperature = dht.readTemperature();
@@ -72,7 +119,10 @@ void loop() {
     display.print(" *C");
     
     display.display();
+}
 
-    // Čekání před dalším čtením
-    delay(2000);
+void setRGB(int red, int green, int blue) {
+    analogWrite(RED_PIN, red);
+    analogWrite(GREEN_PIN, green);
+    analogWrite(BLUE_PIN, blue);
 }
